b55103fac192a270e78d04e0c0cfb49b8a1f5c30
Зайнагтдинова Галия Нафисовна <GZaynagtdinova@beeline.ru>
2024-01-31 09:32:42 +0300

    SFDM-1289 : создание репозитория

---
+++ src/main/java/ri/beeline/cxbackend/CxBackendApplication.java
@@ -0,0 +1,13 @@
+package ri.beeline.cxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CxBackendApplication.class, args);
+    }
+
+}
