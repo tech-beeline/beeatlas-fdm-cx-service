b08982eb24b331c29264347c76b043d16691c82e
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2024-02-12 12:53:42 +0300

    SFDM-1302 add logs

--- src/main/java/ru/beeline/cxbackend/controller/HeaderInterceptor.java
+++ src/main/java/ru/beeline/cxbackend/controller/HeaderInterceptor.java
@@ -1,5 +1,7 @@
 package ru.beeline.cxbackend.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import ru.beeline.cxbackend.exception.UnauthorizedException;
 
@@ -14,24 +16,36 @@ import java.util.stream.Collectors;
 import static ru.beeline.cxbackend.utils.Constant.*;
 
 public class HeaderInterceptor implements HandlerInterceptor {
+    private Logger logger = LoggerFactory.getLogger(HeaderInterceptor.class);
+
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         try {
             Map<String, Object> headers = new HashMap<>();
+            logger.info(USER_ID_HEADER + request.getHeader(USER_ID_HEADER));
             headers.put(USER_ID_HEADER, request.getHeader(USER_ID_HEADER));
+            logger.info(USER_PERMISSION_HEADER + toList(request.getHeader(USER_PERMISSION_HEADER)));
             headers.put(USER_PERMISSION_HEADER, toList(request.getHeader(USER_PERMISSION_HEADER)));
+            logger.info(USER_PRODUCTS_IDS_HEADER + toList(request.getHeader(USER_PRODUCTS_IDS_HEADER)));
             headers.put(USER_PRODUCTS_IDS_HEADER, toList(request.getHeader(USER_PRODUCTS_IDS_HEADER)));
+            logger.info(USER_ROLES_HEADER + toList(request.getHeader(USER_ROLES_HEADER)));
             headers.put(USER_ROLES_HEADER, toList(request.getHeader(USER_ROLES_HEADER)));
             RequestContext.setHeaders(headers);
+            logger.info("Set headers complete");
+            return true;
         } catch (Exception e) {
             new UnauthorizedException("401 " + "Создавать CJ могут только авторизованные пользователи");
+            logger.info("401 " + "Создавать CJ могут только авторизованные пользователи");
+            return false;
         }
-        return true;
     }
 
     private List<String> toList(String value) {
         return Arrays.stream(value.split(","))
+                .map(str -> str.substring(1))
+                .map(str -> str.replace("\"",""))
+                .map(str -> str.substring(0, str.length() - 1))
                 .map(String::trim)
                 .collect(Collectors.toList());
     }
