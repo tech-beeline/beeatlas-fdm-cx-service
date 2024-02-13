237c58de90ab97f957cafead3dfb0891a5f9ef2f
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2024-02-13 10:45:40 +0300

    SFDM-1302 drop logs

--- src/main/java/ru/beeline/cxbackend/controller/HeaderInterceptor.java
+++ src/main/java/ru/beeline/cxbackend/controller/HeaderInterceptor.java
@@ -20,28 +20,16 @@ public class HeaderInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         try {
             Map<String, Object> headers = new HashMap<>();
-            Enumeration<String> headerNames = request.getHeaderNames();
-            while (headerNames.hasMoreElements()) {
-                String headerName = headerNames.nextElement();
-                String headerValue = request.getHeader(headerName);
-                System.out.println(headerName + ": " + headerValue);
-            }
-            logger.info(USER_ID_HEADER + request.getHeader(USER_ID_HEADER));
             headers.put(USER_ID_HEADER, request.getHeader(USER_ID_HEADER));
-            logger.info(USER_PERMISSION_HEADER + toList(request.getHeader(USER_PERMISSION_HEADER)));
             headers.put(USER_PERMISSION_HEADER, toList(request.getHeader(USER_PERMISSION_HEADER)));
-            logger.info(USER_PRODUCTS_IDS_HEADER + toList(request.getHeader(USER_PRODUCTS_IDS_HEADER)));
             headers.put(USER_PRODUCTS_IDS_HEADER, toList(request.getHeader(USER_PRODUCTS_IDS_HEADER)));
-            logger.info(USER_ROLES_HEADER + toList(request.getHeader(USER_ROLES_HEADER)));
             headers.put(USER_ROLES_HEADER, toList(request.getHeader(USER_ROLES_HEADER)));
             RequestContext.setHeaders(headers);
-            logger.info("Set headers complete");
-            return true;
         } catch (Exception e) {
-            new UnauthorizedException("401 " + "Создавать CJ могут только авторизованные пользователи");
             logger.info("401 " + "Создавать CJ могут только авторизованные пользователи");
-            return false;
+            new UnauthorizedException("401 " + "Создавать CJ могут только авторизованные пользователи");
         }
+        return true;
     }
 
     private List<String> toList(String value) {
