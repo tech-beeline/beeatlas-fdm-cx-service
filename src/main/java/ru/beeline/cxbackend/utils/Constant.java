ee9698e843e124e4f89ee2961b52bb75512bdc52
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2024-03-06 10:26:32 +0300

    SFDM-1278 fix headers

--- src/main/java/ru/beeline/cxbackend/utils/Constant.java
+++ src/main/java/ru/beeline/cxbackend/utils/Constant.java
@@ -1,8 +1,8 @@
 package ru.beeline.cxbackend.utils;
 
 public class Constant {
-    public static final String USER_ID_HEADER = "user-id";
-    public static final String USER_PERMISSION_HEADER  = "user-permission";
-    public static final String USER_PRODUCTS_IDS_HEADER = "user-products-ids";
-    public static final String USER_ROLES_HEADER = "user-roles";
+    public static final String USER_ID_HEADER = "USER_ID";
+    public static final String USER_PERMISSION_HEADER  = "USER_PERMISSION";
+    public static final String USER_PRODUCTS_IDS_HEADER = "USER_PRODUCTS_IDS";
+    public static final String USER_ROLES_HEADER = "USER_ROLES";
 }
