25a79fd0a00a7d4df15f8441d6d77a3a3963d24a
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2024-07-11 12:24:38 +0300

    SFDM-1651 Исправить работу методов CX через gateway

--- src/main/java/ru/beeline/cxbackend/utils/AccessToProduct.java
+++ src/main/java/ru/beeline/cxbackend/utils/AccessToProduct.java
@@ -12,17 +12,17 @@ import static ru.beeline.cxbackend.domain.Permission.PermissionType.DESIGN_ARTIF
 public class AccessToProduct {
 
     public static void validateAccessProduct(List<String> permissions, List<Long> product, Long productId) {
-        if (!product.contains(productId) && !permissions.contains(Permission.PermissionType.DESIGN_ARTIFACT.toString()))
+        if (!(product.contains(productId) || permissions.contains(Permission.PermissionType.DESIGN_ARTIFACT.toString())))
             throw new ForbiddenException("FORBIDDEN");
     }
 
     public static void validateAccessProduct(List<String> permissions, List<Long> product, BI bi) {
-        if (bi.isDraft() && !product.contains(bi.getProductId()) && !permissions.contains(DESIGN_ARTIFACT.toString()))
+        if (!(bi.isDraft() && product.contains(bi.getProductId()) || permissions.contains(DESIGN_ARTIFACT.toString())))
             throw new ForbiddenException("FORBIDDEN");
     }
 
     public static void validateAccessProduct(List<String> permissions, List<Long> product, CJ cj) {
-        if (cj.isBDraft() && !product.contains(cj.getIdProductExt()) && !permissions.contains(DESIGN_ARTIFACT.toString()))
+        if (!(cj.isBDraft() && product.contains(cj.getIdProductExt()) || permissions.contains(DESIGN_ARTIFACT.toString())))
             throw new ForbiddenException("FORBIDDEN");
     }
 }
