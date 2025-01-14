b03788e884bc255e08a525fce914786a4451f83b
salikhanov <SAlikhanov@oms.beeline.ru>
2025-01-14 13:42:25 +0600

    SFDM-2227 Доработать метод получение, создание, редактирования CJ

--- src/main/java/ru/beeline/cxbackend/dto/BIDto.java
+++ src/main/java/ru/beeline/cxbackend/dto/BIDto.java
@@ -1,7 +1,6 @@
 package ru.beeline.cxbackend.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,12 +32,9 @@ public class BIDto {
     private Long productId;
     private String ownerRole;
     private String metrics;
-    @JsonProperty("author_id")
     private Long authorId;
-    @JsonProperty("created_date")
     @JsonFormat(pattern = DATE_FORMAT, timezone = DATE_TIMEZONE)
     private Date createdDate;
-    @JsonProperty("last_modified_date")
     @JsonFormat(pattern = DATE_FORMAT, timezone = DATE_TIMEZONE)
     private Date lastModifiedDate;
     private BIStatusDto status;
