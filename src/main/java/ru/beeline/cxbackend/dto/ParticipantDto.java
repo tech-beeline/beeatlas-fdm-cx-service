2043253cc09f3798040f048488ff9d16b704bff1
salikhanov <SAlikhanov@oms.beeline.ru>
2025-08-25 22:35:40 +0600

    SFDM-2822  Убрать атрибут idBPMN из тела ответа методов CX. Рефакторинг

---
+++ src/main/java/ru/beeline/cxbackend/dto/ParticipantDto.java
@@ -0,0 +1,17 @@
+package ru.beeline.cxbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParticipantDto {
+
+    private Long idType;
+    private String value;
+    private String descr;
+}
