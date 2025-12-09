b39975bf62fdd4a91a21055261ad76701b25a234
salikhanov <SAlikhanov@oms.beeline.ru>
2025-12-09 18:20:40 +0600

    SFDM-3169 Доработать метод POST /api/cx/v1/bpmn/cj/{cj-id} заполнения атрибутов CJ

--- src/main/java/ru/beeline/cxbackend/client/DocumentClient.java
+++ src/main/java/ru/beeline/cxbackend/client/DocumentClient.java
@@ -3,12 +3,18 @@ package ru.beeline.cxbackend.client;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import ru.beeline.cxbackend.controller.RequestContext;
+import ru.beeline.cxbackend.dto.DocumentationTypeDTO;
 import ru.beeline.cxbackend.exception.NotFoundException;
+
+import java.util.List;
 
 import static ru.beeline.cxbackend.utils.Constant.USER_ID_HEADER;
 import static ru.beeline.cxbackend.utils.Constant.USER_ROLES_HEADER;
@@ -27,19 +33,41 @@ public class DocumentClient {
         this.restTemplate = restTemplate;
     }
 
-    public ResponseEntity<byte[]> getDocument(Long docId) {
+    public ResponseEntity<byte[]> getDocument(Long docId, Integer documentationTypeId) {
         try {
             HttpHeaders headers = new HttpHeaders();
             headers.set(USER_ID_HEADER, RequestContext.getUserId().toString());
             headers.set(USER_ROLES_HEADER, RequestContext.getUserRole().toString());
             HttpEntity<String> entity = new HttpEntity<>(headers);
 
-            return restTemplate.exchange(documentServiceUrl + "/api/v1/documents/6/" + docId,
-                                         HttpMethod.GET,
-                                         entity,
-                                         new ParameterizedTypeReference<byte[]>() {});
+            return restTemplate.exchange(documentServiceUrl + "/api/v1/documents/" + documentationTypeId + "/" + docId,
+                    HttpMethod.GET,
+                    entity,
+                    new ParameterizedTypeReference<byte[]>() {
+                    });
         } catch (HttpClientErrorException.NotFound e) {
             log.error("Запись с данным id не найдена: ", e);
+            throw new NotFoundException(e.getMessage());
+        } catch (Exception e) {
+            log.error("Exception occurred: ", e);
+            throw e;
+        }
+    }
+
+    public List<DocumentationTypeDTO> getDocumentationType(String entityType) {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set(USER_ID_HEADER, RequestContext.getUserId().toString());
+            headers.set(USER_ROLES_HEADER, RequestContext.getUserRole().toString());
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            return restTemplate.exchange(documentServiceUrl + "/api/v1/documentations/" + entityType,
+                    HttpMethod.GET,
+                    entity,
+                    new ParameterizedTypeReference<List<DocumentationTypeDTO>>() {
+                    }).getBody();
+        } catch (HttpClientErrorException.NotFound e) {
+            log.error("Запись с  не entityType: {} найдена: ", entityType, e);
             throw new NotFoundException(e.getMessage());
         } catch (Exception e) {
             log.error("Exception occurred: ", e);
